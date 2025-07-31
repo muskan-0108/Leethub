89f1326a975ae9eeb563cf4bf8a54294027597a5
muskan-0108 <muskanraj0108@gmail.com>
2025-07-31 10:33:49 +0530

    Time: 15 ms (2.88%), Space: 45.1 MB (35.29%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -14,35 +14,30 @@
  * }
  */
 class Solution {
+    public int ht(TreeNode root){
+        if(root==null || root.left==null && root.right==null) return 0;
+        return 1+Math.max(ht(root.left),ht(root.right));
+    }
+    public void nthnode1(TreeNode root,int n,List<Integer> ans){//left to right
+        if(root==null) return;
+        if(n==1){
+            ans.add(root.val);
+            return;
+        }
+        nthnode1(root.left,n-1,ans);
+        nthnode1(root.right,n-1,ans);
+    }
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        List<List<Integer>> ans = new LinkedList<List<Integer>>();
-        if(root==null) return ans;
-        q.add(root);
-        q.add(null);
+        List<List<Integer>> Fans = new ArrayList<>();
+        if(root==null) return Fans;
+        int level = ht(root)+1;//ht(root) is for edge
+        for(int i=1;i<=level;i++){
+            List<Integer> ans = new ArrayList<>();
+                nthnode1(root,i,ans);
+            Fans.add(ans);
+        }
+        return Fans;
 
-        List<Integer> level = new ArrayList<>();
-
-        while(!q.isEmpty()){
-            TreeNode currNode = q.remove();
-            if(currNode==null){
-                ans.add(level);
-                level = new ArrayList<>();
-                if(q.isEmpty()){
-                    break;
-                }else{
-                    q.add(null);
-                }
-            }else{
-                level.add(currNode.val);
-                if(currNode.left!=null){
-                    q.add(currNode.left);
-                }
-                if(currNode.right!=null){
-                    q.add(currNode.right);
-                }
-            }
-        }  
-        return ans;  
+        
     }
 }
