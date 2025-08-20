79de534557a17001ee96418c09c27fabd71afd5b
muskan-0108 <muskanraj0108@gmail.com>
2025-08-21 00:35:01 +0530

    Time: 5 ms (76.22%), Space: 58.2 MB (32.16%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = h * width;
+            maxArea = Math.max(maxArea, area);
+
+            // Move the smaller line pointer
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
