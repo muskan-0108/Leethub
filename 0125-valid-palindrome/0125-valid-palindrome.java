e440393a9fdba4f06b0a68b73c915fd26a8e474c
muskan-0108 <muskanraj0108@gmail.com>
2026-02-19 01:50:54 +0530

    Time: 3 ms (62.65%), Space: 44.9 MB (46.84%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase().replaceAll("[^a-z0-9]","");
-        int left=0;
-        int right=s.length()-1;
-
-        while(left<right){
-            if(s.charAt(left)!=s.charAt(right)){
+        String ls = s.toLowerCase();
+        char[] la=ls.toCharArray();
+        int i=0;
+        int j=la.length-1;
+        while(i<j){
+            if(!Character.isLetterOrDigit(la[i])){
+                i++;
+            }else if(!Character.isLetterOrDigit(la[j])){
+                j--;
+            }else if(la[i]!=la[j]){
                 return false;
+            }else {
+                i++;
+                j--;
             }
-            left++;
-            right--;
         }
         return true;
-        
     }
 }
