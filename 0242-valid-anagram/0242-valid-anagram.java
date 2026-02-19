553dc1e6dc8bcc873889ca0265605e9d81896842
muskan-0108 <muskanraj0108@gmail.com>
2026-02-19 17:01:23 +0530

    Time: 4 ms (80.24%), Space: 44.6 MB (67.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-
-        char[] s1 = s.toCharArray();
-        char[] s2 = t.toCharArray();
-
-        Arrays.sort(s1);
-        Arrays.sort(s2);
-
-        return Arrays.equals(s1,s2);
+        int[] freq1=new int[26];
+        int[] freq2=new int[26];
+        if(s.length()==t.length()){
+            for(int i=0;i<s.length();i++){
+                int idx=s.charAt(i)-'a';
+                freq1[idx]++;
+            }
+            for(int i=0;i<t.length();i++){
+                int idx=t.charAt(i)-'a';
+                freq2[idx]++;
+            }
+            for(int i=0;i<=25;i++){
+            if(freq1[i]!=freq2[i]){
+                return false;
+            }
+            }
+            return true;
+        }
+        return false;
         
     }
 }
